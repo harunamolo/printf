6d31b1aaf26f4c910aad0e0f6f434eaef4488d4d
harunamolo <107302226+harunamolo@users.noreply.github.com>
2022-08-05 15:19:51 +0300

    Update printpercent.c

--- printpercent.c
+++ printpercent.c
@@ -1,1 +1,16 @@
-file 7
+#include "main.h"
+
+/**
+ * printpercent - prints %
+ * @format: format
+ * @pa: va_list
+ * Return: number of characters printed
+ */
+int printpercent(char *format, va_list pa)
+{
+	(void)format;
+	(void)pa;
+	_putchar('%');
+	return (1);
+}
+
